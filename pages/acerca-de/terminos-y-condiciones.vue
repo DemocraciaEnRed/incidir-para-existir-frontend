ac4f2deeca56973fe1274ea4c8704320d23b2daf
Guillermo Croppi <guillermocroppi@gmail.com>
2024-12-19 20:27:55 -0300

    Add Terms and Conditions page with dynamic content

---
+++ pages/acerca-de/terminos-y-condiciones.vue
@@ -0,0 +1,17 @@
+<script setup>
+  const { data: configs } = await useAPI(`/utils/configs?keys=tos`)
+
+</script>
+<template>
+  <UContainer>
+    <h3 class="text-center font-oswald text-2xl lg:text-5xl text-mindaro my-20">Terminos y condiciones</h3>
+    <div class="text-white">
+      {{ configs.tos }}
+    </div>
+  </UContainer>
+</template>
+
+
+<style>
+
+</style>
