38e07106675f2a23f1fed24673da027cc8a4085f
Guillermo Croppi <guillermocroppi@gmail.com>
2024-12-26 15:27:43 -0300

    Refactor footer component for improved responsiveness and dynamic styling

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
+<script setup>
 
+const route = useRoute()
+
+const rounded = computed(() => {
+  // based on the path, if it includes the followings, return false
+  const disableRoundedFooterForPaths = [
+    '/quienes-somos',
+    '/iniciativas',
+    '/actualidad',
+  ]
+
+  return !disableRoundedFooterForPaths.includes(route.path)
+})
 </script>
 
 <template>
-  <footer class="the-footer">
+  <footer class="the-footer" :class="rounded ? 'rounded-tl-xl rounded-tr-xl' : null">
     <div class="starbg"/>
     <UContainer class="the-content">
       <div class="flex justify-center items-center my-5 flex-col md:flex-row space-y-5 md:space-y-0">
@@ -12,11 +24,11 @@
         <img src="/img/movilizatorio-logo.png" alt="Movilizatorio" class="h-8 px-6">
       </div>
       <hr>
-      <div class="grid grid-cols-1 sm:grid-cols-4 gap-5 my-10 font-oswald uppercase">
-        <NuxtLink to="/quienes-somos" class="text-center hover:text-mindaro">¿Quiénes somos?</NuxtLink>
-        <NuxtLink to="/iniciativas" class="text-center hover:text-mindaro">Iniciativas de la juventud</NuxtLink>
-        <NuxtLink to="/desafios" class="text-center hover:text-mindaro">Desafios de la juventud</NuxtLink>
-        <NuxtLink to="/noticias" class="text-center hover:text-mindaro">¿En qué está la juventud?</NuxtLink>
+      <div class="flex flex-col justify-center gap-5 md:flex-row my-10 font-oswald uppercase items-center">
+        <NuxtLink to="/quienes-somos" class="text-center leading-none hover:text-mindaro">¿Quiénes somos?</NuxtLink>
+        <NuxtLink to="/iniciativas" class="text-center leading-none hover:text-mindaro">Iniciativas <br class="hidden sm:block"><span class="sm:text-sm font-normal">de la juventud</span></NuxtLink>
+        <NuxtLink to="/desafios" class="text-center leading-none hover:text-mindaro">Desafios <br class="hidden sm:block"><span class="sm:text-sm font-normal">de la juventud</span></NuxtLink>
+        <NuxtLink to="/noticias" class="text-center leading-none hover:text-mindaro">Actualidad <br class="hidden sm:block"><span class="sm:text-sm font-normal">de la juventud</span></NuxtLink>
       </div>
     </UContainer>
   </footer>
