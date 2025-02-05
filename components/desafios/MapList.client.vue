0907a64f2dc8128f5697430e5dd85a30d0ade52d
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-05 16:46:26 -0300

    Add MapList component for displaying challenges on a map; update desafios page layout

---
+++ components/desafios/MapList.client.vue
@@ -0,0 +1,122 @@
+<script setup>
+import { Marker, LatLng, LatLngBounds } from 'leaflet';
+import { DesafiosDetail } from '#components'
+
+const { $api } = useNuxtApp();
+
+const map = ref(null)
+
+const slideover = useSlideover()
+const selectedCity = ref(null)
+const defaultZoom = 10
+const zoom = ref(defaultZoom)
+const mapReady = ref(false)
+
+// const { data } = useAPI('/challenges/list/geolocalized')
+
+const { data, status } = await useAsyncData('map-challenges', async () => {
+  const [cities, challenges] = await Promise.all([
+    $api('/utils/cities'),
+    $api('/challenges/list/geolocalized'),
+  ])
+
+  return { cities, challenges }
+})
+
+const isLoading = computed(() => status.value === 'pending');
+
+
+// When the map is ready
+const onMapReady = () => {
+    // Access the Leaflet map instance
+    console.log(map.value.leafletObject)
+    // Add a marker to the map
+    data.value.challenges.forEach(challenge => {
+      const marker = new Marker([challenge.latitude, challenge.longitude])
+      marker.bindTooltip(getTooltipHtml(challenge),{
+        className: 'custom-tooltip'
+      })
+      marker.on('click', () => {
+        openChallengeDetails(challenge.id)
+      })
+      marker.addTo(map.value.leafletObject)
+    });
+    centerMapToCity('Cali')
+    // set mapReady to true
+    mapReady.value = true
+} 
+
+const getTooltipHtml = (challenge) => {
+  return `
+    <div>
+      <p class="font-inter text-xs text-white">Reporte <span class="text-mindaro"># ${addLeadingZeros(challenge.id)}</span></p>
+      <p class="font-inter text-mindaro text-xs my-1">${challenge.dimension.name}</p>
+      <h1 class="font-inter text-gray-400 leading-tight text-xs italic my-1">${challenge.needsAndChallenges}</h1>
+      <p class="font-inter text-white text-xs"><span class="uppercase">${challenge.subdivision.city.name}</span> | ${challenge.subdivision.type} ${challenge.subdivision.name}</p>
+
+    </div>
+  `
+}
+
+const openChallengeDetails = (id) => {
+  slideover.open(DesafiosDetail, {
+    desafioId: id,
+    onClose: slideover.close
+  })
+}
+
+const centerMapToCity = (city) => {
+  const cityData = data.value.cities.find(c => c.name === city)
+  selectedCity.value = cityData
+  // map.value.leafletObject.fitBounds([southWestCoordinates, northEastCoordinates])
+  const southWestCoordinates = [cityData.southWestLatitude, cityData.southWestLongitude]
+  const northEastCoordinates = [cityData.northEastLatitude, cityData.northEastLongitude]
+  map.value.leafletObject.setMaxBounds([southWestCoordinates, northEastCoordinates])
+  map.value.leafletObject.setView([cityData.latitude, cityData.longitude], defaultZoom)
+}
+
+const getCityButtonVariant = (city) => {
+  if(selectedCity.value && selectedCity.value.name === city) {
+    return 'solid'
+  }
+  return 'outline'
+}
+
+
+</script>
+
+<template>
+  <div>
+    <div>
+      <div class="flex flex-col items-center">
+        <p>Seleccione una ciudad para ver los desafios</p>
+        <div class="flex justify-center space-x-3 my-4">
+          <UButton class="text-xl" :variant="getCityButtonVariant('Cali')" size="lg" color="mindaro" :loading="!mapReady"  :disabled="!mapReady" :ui="{ rounded: 'rounded-full' }"  @click="centerMapToCity('Cali')">Cali</UButton>
+          <UButton class="text-xl" :variant="getCityButtonVariant('Bogotá')" size="lg" color="mindaro" :loading="!mapReady" :disabled="!mapReady" :ui="{ rounded: 'rounded-full' }"  @click="centerMapToCity('Bogotá')">Bogotá</UButton>
+        </div>
+      </div>
+    </div>
+    <USkeleton v-if="isLoading" class="w-full h-[500px] rounded-lg" />
+    <div v-else class="w-full h-[500px] rounded-lg bg-slate-700">
+      <LMap
+        ref="map"
+        class="rounded-lg z-20"
+        :zoom="zoom"
+        :min-zoom="9"
+        :max-bounds-viscosity="0.2"
+        :use-global-leaflet="false"
+        @ready="onMapReady"
+      >
+        <!-- <LControlZoom position="bottomright" /> -->
+        <LTileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="Movilizatorio <a href=&quot;mailto:hola@movilizatorio.org&quot; target=&quot;blank&quot;>✉️</a> | &amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot; target=&quot;blank&quot;>OpenStreetMap</a> contributors"
+          layer-type="base"
+          name="OpenStreetMap"
+        />
+
+      </LMap>
+    </div>
+  </div>
+
+</template>
--- pages/desafios.vue
+++ pages/desafios.vue
@@ -4,6 +4,9 @@
 useHead({
   title: 'Desafios de la juventud'
 })
+definePageMeta({
+  layout: 'full-width'
+})
 
 const showDesafiosForm = ref(false)
 
@@ -27,46 +30,59 @@ const showDesafiosForm = ref(false)
 </script>
 
 <template>
-  <UContainer>
-    <div class="flex gap-5 justify-between items-center my-10 flex-col md:flex-row">
-      <div class="w-full lg:w-1/2 text-center">
-        <p class="font-oswald text-5xl uppercase leading-tight text-mindaro mb-5">Entérate de los desafíos de las juventudes en tu ciudad y déjanos saber cuáles son tus propuestas</p>
-        <p class="font-bold text-xl leading-tight mb-5">Los desafíos de las juventudes son las problemáticas y barreras que limitan su acceso a derechos, oportunidades y espacios seguros para participar y transformar su entorno.</p>
+  <div>
+    <UContainer>
+      <div class="flex gap-5 justify-between items-center my-10 flex-col md:flex-row">
+        <div class="w-full lg:w-1/2 text-center">
+          <p class="font-oswald text-5xl uppercase leading-tight text-mindaro mb-5">Entérate de los desafíos de las juventudes en tu ciudad y déjanos saber cuáles son tus propuestas</p>
+          <p class="font-bold text-xl leading-tight mb-5">Los desafíos de las juventudes son las problemáticas y barreras que limitan su acceso a derechos, oportunidades y espacios seguros para participar y transformar su entorno.</p>
 
+        </div>
+        <div class="w-full lg:w-1/2">
+          <img src="/img/desafios-01.png" alt="Incidir para existir" class="w-full lg:w-4/5 mx-auto rounded-lg">
+        </div>
       </div>
-      <div class="w-full lg:w-1/2">
-        <img src="/img/desafios-01.png" alt="Incidir para existir" class="w-full lg:w-4/5 mx-auto rounded-lg">
+      <div class="my-10 w-full md:w-4/5 mx-auto space-y-5">
+        <UButton v-if="!showDesafiosForm" color="pumpkin" block size="xl" class="text-center text-xl sm:text-2x lg:text-3xl font-weight font-inter font-black rounded-full" @click="showDesafiosForm = true">Quiero reportar desafíos y propuestas de la juventud</UButton>
+        <p class="font-center text-lg leading-tight text-center">Recuerda que los <b>datos tienen poder</b>. Las propuestas y reportes que compartas serán analizados y utilizados para visibilizar los desafíos de las juventudes. Esta información buscará sensibilizar a diferentes actores clave y promover una mayor atención a las necesidades planteadas</p>
+      </div>
+      <div v-if="showDesafiosForm" class="my-10 w-full md:w-2/3 mx-auto">
+        <DesafiosForm />
+      </div>
+    </UContainer> 
+    <div class="bg-electricViolet-800 py-10 rounded-tr-2xl rounded-tl-2xl">
+      <UContainer class="">
+        <p class="font-oswald text-5xl leading-tight text-mindaro text-center">MAPA DE DESAFIOS</p>
+        <DesafiosMapList />
+      </UContainer>
+    </div>
+    <UContainer>
+      <div class="my-10">
+      <p class="font-oswald text-4xl text-center uppercase leading-tight text-mindaro mb-5">Estadisticas</p>
+      <ClientOnly>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+          <DesafiosChartsDimensions class="md:col-span-2" />
+          <DesafiosChartsSubdivisionsCali class="" />
+          <DesafiosChartsSubdivisionsBogota class="" />
+        </div>
+        <template #fallback>
+          <!-- this will be rendered on server side -->
+          <UCard>
+            <UProgress />
+          </UCard>
+        </template>
+      </ClientOnly>
+      <div class="space-y-7">
+        <div class="text-center">
+          <p class="font-oswald text-5xl leading-tight text-mindaro text-center my-10">REPORTES DE DESAFIOS</p>
+        </div>
+        <DesafiosList />
       </div>
     </div>
-    <div class="my-10 w-full md:w-4/5 mx-auto space-y-5">
-      <UButton v-if="!showDesafiosForm" color="pumpkin" block size="xl" class="text-center text-xl sm:text-2x lg:text-3xl font-weight font-inter font-black rounded-full" @click="showDesafiosForm = true">Quiero reportar desafíos y propuestas de la juventud</UButton>
-      <p class="font-center text-lg leading-tight text-center">Recuerda que los <b>datos tienen poder</b>. Las propuestas y reportes que compartas serán analizados y utilizados para visibilizar los desafíos de las juventudes. Esta información buscará sensibilizar a diferentes actores clave y promover una mayor atención a las necesidades planteadas</p>
-    </div>
-    <div v-if="showDesafiosForm" class="my-10 w-full md:w-2/3 mx-auto">
-      <DesafiosForm />
-    </div>
-  <UDivider size="md" class="my-5" />
-    <p class="font-oswald text-4xl text-center uppercase leading-tight text-mindaro mb-5">Estadisticas</p>
-    <ClientOnly>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <DesafiosChartsDimensions class="md:col-span-2" />
-        <DesafiosChartsSubdivisionsCali class="" />
-        <DesafiosChartsSubdivisionsBogota class="" />
-      </div>
-      <template #fallback>
-        <!-- this will be rendered on server side -->
-        <UCard>
-          <UProgress />
-        </UCard>
-      </template>
-    </ClientOnly>
-    <div class="space-y-7">
-      <div class="text-center">
-        <p class="font-oswald text-5xl leading-tight text-mindaro text-center my-10">REPORTES DE DESAFIOS</p>
-      </div>
-      <DesafiosList />
-    </div>
-  </UContainer> 
+
+    </UContainer>
+
+  </div>
 </template>
 
 
