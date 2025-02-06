be95c01588d9bcec227926b43c140379190d460b
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-05 21:03:06 -0300

    Refactor LocalizationSelector component: consolidate functionality and improve loading state management

---
+++ components/LocalizationSelector.vue
@@ -0,0 +1,94 @@
+<script setup>
+
+  const { $api } = useNuxtApp();
+
+  const toast = useToast()
+
+  const selectedCity = defineModel('selectedCity', {
+    type: [Object, null],
+    required: true,
+    default: () => null
+  })
+
+  const selectedSubdivision = defineModel('selectedSubdivision', {
+    type: [Object, null],
+    required: true,
+    default: () => null
+  })
+
+  const isLoading = ref(true)
+  const cities = ref([]) 
+
+  const fetchData = async () => {
+    isLoading.value = true
+    try {
+      const data = await $api('/utils/cities',{
+        method: 'GET'
+      })
+      cities.value = data
+    } catch (error) {
+      console.error(error)
+      toast.add({ title: 'Error', description: 'Hubo un error al cargar las ciudades', color: 'red' })
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  const subdivisionsOptions = computed(() => {
+    if(!selectedCity.value) return []
+    return selectedCity.value.subdivisions
+  })
+
+  const clearCityFilter = () => {
+    selectedCity.value = null
+    selectedSubdivision.value = null
+  }
+
+  const clearSubdivisionFilter = () => {
+    selectedSubdivision.value = null
+  }
+
+  watch([selectedCity], () => {
+    selectedSubdivision.value = null
+  })
+
+  const clearCityDisabled = computed(() => {
+    return selectedCity.value === null
+  })
+
+  const clearSubdivisionDisabled = computed(() => {
+    return selectedSubdivision.value === null || selectedCity.value === null
+  })
+
+  // lifecycle hook
+  onMounted(() => {
+    fetchData()
+  })
+
+</script>
+
+<template>
+  <div class="flex gap-2 w-full md:w-8/12 md:mx-auto">
+  <div class="flex gap-2 w-1/2 mx-auto">
+    <USelectMenu color="mindaro" v-model="selectedCity" :options="cities" option-attribute="name" :loading="isLoading" placeholder="Filtrar por ciudad" size="lg" class="w-full" :ui-menu="{ container: 'z-[1500] group' }"/>
+    <UButton color="white" :disabled="clearCityDisabled" variant="ghost" @click="clearCityFilter">
+      <UIcon name="i-heroicons-backspace" class="text-lg"/>
+    </UButton>
+  </div>
+  <div class="flex gap-2 w-1/2 mx-auto" v-if="selectedCity">
+      <USelectMenu color="mindaro" v-model="selectedSubdivision" :options="subdivisionsOptions" :loading="isLoading" placeholder="Filtrar por ubicación" size="lg" class="w-full" :ui-menu="{ container: 'z-[1500] group' }">
+        <template #option="{ option }">
+          <span>{{ option.type }} {{ option.name }}</span>
+        </template>
+        <template #label>
+          <span v-if="selectedSubdivision">{{ selectedSubdivision.type }} {{ selectedSubdivision.name }}</span>
+          <span v-else>Filtrar por ubicación</span>
+        </template>
+      </USelectMenu>
+      <UButton color="white" :disabled="clearSubdivisionDisabled" variant="ghost" @click="clearSubdivisionFilter">
+        <UIcon name="i-heroicons-backspace" class="text-lg"/>
+      </UButton>
+  </div>
+</div>
+
+</template>
