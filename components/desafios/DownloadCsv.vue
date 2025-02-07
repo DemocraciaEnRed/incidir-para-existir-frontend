e4e93397d0e43ec5dbcc9f4cd4765e52472bdeb7
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-06 23:10:53 -0300

    Add CSV download functionality for challenges and initiatives with reCAPTCHA validation

---
+++ components/desafios/DownloadCsv.vue
@@ -0,0 +1,62 @@
+<script setup>
+
+import { RecaptchaModal } from '#components'
+
+const { $api } = useNuxtApp()
+
+const modal = useModal()
+const toast = useToast()
+
+const recaptchaResponse = ref(null)
+const isLoading = ref(false)
+
+const startRecaptchaChallenge = async () => {
+  modal.open(RecaptchaModal, {
+    onSubmitRecaptcha: (response) => {
+      recaptchaResponse.value = response 
+      handleSubmit()
+      modal.close()
+    },
+    onCloseModal: () => {
+      modal.close()
+    }
+  })
+}
+
+
+const handleSubmit = async () => {
+  isLoading.value = true
+  try{
+    // because this needs to be send with the bearer token
+    const url = '/challenges/csv'
+    const payload = {
+      recaptchaResponse: recaptchaResponse.value
+    }
+    const {_data, headers } = await $api.raw(url, {
+      method: 'POST',
+      body: JSON.stringify(payload)
+    })
+    // extract from content-disposition the filename
+    const filename = headers.get('content-disposition').split('filename=')[1]
+    
+    // download the csv
+    const blob = new Blob([_data], { type: 'text/csv' })
+    const urlBlob = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = urlBlob
+    a.download = filename
+    a.click()
+    recaptchaResponse.value = null
+    isLoading.value = false
+  } catch (error) {
+    console.log(error)
+    toast.add({ title: 'Error', description: 'Hubo un error al descargar el CSV', color: 'red' })
+    isLoading.value = false
+  }
+}
+
+</script>
+
+<template>
+  <UButton color="mindaro" class="text-xl font-medium" :loading="isLoading" size="lg" block icon="i-heroicons-arrow-down-tray" :ui="{ rounded: 'rounded-full' }" @click="startRecaptchaChallenge">Descargar CSV</UButton>
+</template>
--- pages/admin/desafios.vue
+++ pages/admin/desafios.vue
@@ -1,4 +1,8 @@
-<script lang="ts" setup>
+<script setup>
+
+const { $api } = useNuxtApp();
+const toast = useToast();
+
 const links = [{
   label: 'Listado',
   icon: 'i-heroicons-list-bullet',
@@ -9,7 +13,35 @@ const links = [{
   icon: 'i-heroicons-plus',
   to: '/admin/desafios/crear',
   exact: true
+}, {
+  label: 'Descargar CSV',
+  icon: 'i-heroicons-arrow-down-tray',
+  click: () => downloadCSV()
 }]
+
+const downloadCSV = async () => {
+  console.log('Downloading CSV')
+  try{
+    // because this needs to be send with the bearer token
+    const url = '/admin/challenges/csv'
+    const {_data, headers } = await $api.raw(url, {
+      method: 'GET'
+    })
+    // extract from content-disposition the filename
+    const filename = headers.get('content-disposition').split('filename=')[1]
+    
+    // download the csv
+    const blob = new Blob([_data], { type: 'text/csv' })
+    const urlBlob = window.URL.createObjectURL(blob)
+    const a = document.createElement('a')
+    a.href = urlBlob
+    a.download = filename
+    a.click()
+  } catch (error) {
+    console.log(error)
+    toast.add({ title: 'Error', description: 'Hubo un error al descargar el CSV', color: 'red' })
+  }
+}
 </script>
 
 <template>
