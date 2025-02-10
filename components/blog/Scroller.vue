937a34a4c91a9879279ec2b7492d8c137c8a1e57
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-10 20:45:13 -0300

    Increase blog post limit and enhance layout with improved item display and navigation

--- components/blog/Scroller.vue
+++ components/blog/Scroller.vue
@@ -12,9 +12,8 @@ const props = defineProps({
 const items = ref([])
 const totalItems = ref(0)
 const page = ref(1)
-const limit = ref(2)
+const limit = ref(3)
 const isLoading = ref(true)
-
 
 const { $api } = useNuxtApp()
 
@@ -28,6 +27,9 @@ const getBlogPosts = async () => {
   });
   
 }
+
+const scrollHelper = useTemplateRef(`section${props.section.value}-scroll`)
+
 
 onMounted(() => {
   console.log('Mounted')
@@ -47,10 +49,18 @@ watch(page, (newValue, oldValue) => {
     items.value = data.rows
     totalItems.value = data.count
     isLoading.value = false
+    scrollHelper.value.scrollIntoView({ behavior: 'smooth', block: 'start' })
   }).catch((error) => {
     console.error(error)
   })
 })
+
+const refName = `section${props.section.value}-scroll`
+
+const goTo = (slug) => navigateTo({
+    name: 'blog-post',
+    params: {slug: slug}
+  })
 
 </script>
 
@@ -59,26 +69,36 @@ watch(page, (newValue, oldValue) => {
     <p class="text-center mb-4 text-sm">Cargando...</p>
     <UProgress animation="carousel" color="pumpkin"/>
   </UCard>
-  <div v-else>
-    <div class="flex gap-5  my-10 flex-col md:flex-row">
-      <div v-for="item in items" :key="item.id" class="w-full lg:w-1/2">
-        <div class="aspect-h-2 aspect-w-5 bg-gray-600 w-full rounded-tl-xl rounded-tr-xl" >
+  <div v-else-if="totalItems > 0" class="my-5">
+  
+    <div :ref="refName" class="grid grid-cols-1 md:grid-cols-3 gap-3 my-10 flex-wrap md:flex-row">
+      <div v-for="item in items" :key="item.id" class="w-full">
+        <div class="aspect-h-2 aspect-w-5 bg-gray-600 w-full rounded-tl-xl rounded-tr-xl  border border-white cursor-pointer" @click="goTo(item.slug)">
           <img :src="item.imageUrl" class="object-cover w-full h-full rounded-tl-xl rounded-tr-xl">
         </div>
-        <div class="flex flex-row justify-between items-stretch py-3 px-4 bg-gray-800 w-full rounded-bl-xl rounded-br-xl">
-          <div class="w-2/3 gap-3 space-y-3">
-            <p class="font-oswald text-2xl text-[#D4F779]">{{ item.title }}</p>
-            <p class="font-inter text-sm truncate">{{ item.subtitle }}</p>
-            <UButton :to="{name: 'blog-post', params: {slug: item.slug}}">Ir al post</UButton>
+        <div class="flex flex-col md:min-h-[320px] text-black justify-between py-3 px-4 bg-white w-full rounded-bl-xl rounded-br-xl space-y-3 shadow-md shadow-slate-700">
+          <div class="flex-1 space-y-2 cursor-pointer" @click="goTo(item.slug)">
+            <div class="flex justify-between items-center mb-2"> 
+              <UBadge color="pumpkin" size="sm" variant="solid" :ui="{ rounded: 'rounded-full' }">{{ item.category.name }}</UBadge>
+              <p class="text-gray-500 text-sm">{{ formatDate(item.createdAt) }}</p>
+            </div>
+            <p class="font-oswald font-normal text-purple-500 text-xl uppercase leading-tight transition-colors hover:text-pumpkin-600 delay-100">{{ item.title }}</p>
+            <p class="text-sm overflow-hidden">{{ item.subtitle }}</p>
           </div>
-          <div class="w-1/3 text-right space-y-3">
-            <UBadge color="pumpkin" variant="solid">{{ item.category.name }}</UBadge>
-            <p class="text-sm">{{ item.createdAt.split('T')[0] }}</p>
+          <div class="flex items-center w-full my-5">
+            <UAvatar :src="item.author.imageUrl" size="md" class="mr-3" :alt="item.author.fullName" />
+            <div class="flex flex-col items-start w-full">
+              <p class="font-medium">{{ item.author.fullName }}</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
     <UPagination v-model="page" :page-count="2" :total="totalItems" />
   </div>
+  <div v-else>
+    <p class="text-white text-sm my-10">No hay posts disponibles en esta sección</p>
+  </div>
+  
 </template>
 
