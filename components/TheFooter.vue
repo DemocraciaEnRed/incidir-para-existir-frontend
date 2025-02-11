feda7623b796f3adc9a39f6d5e67a60b5f6d0bbf
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-11 14:43:27 -0300

    Enhance footer component by adding contact link and developer credit, and improving horizontal rule styling

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -25,14 +25,18 @@ const rounded = computed(() => {
         <a href="https://www.un.org/democracyfund/" target="_blank"><img src="/img/undef-logo.png" alt="UNDEF" class="h-11 px-6"></a>
         <a href="https://movilizatorio.org/" target="_blank"><img src="/img/movilizatorio-logo.png" alt="Movilizatorio" class="h-8 px-6"></a>
       </div>
-      <hr>
+      <hr class="opacity-60">
       <div class="flex flex-col justify-center gap-5 md:flex-row my-10 font-oswald uppercase items-center">
         <NuxtLink to="/" active-class="text-mindaro" class="text-center leading-none hover:text-mindaro">Inicio</NuxtLink>
         <NuxtLink to="/quienes-somos" active-class="text-mindaro" class="text-center leading-none hover:text-mindaro">¿Quiénes somos?</NuxtLink>
         <NuxtLink to="/iniciativas"  active-class="text-mindaro" class="text-center leading-none hover:text-mindaro">Iniciativas <br class="hidden sm:block"><span class="sm:text-sm font-normal">de la juventud</span></NuxtLink>
         <NuxtLink to="/desafios"  active-class="text-mindaro" class="text-center leading-none hover:text-mindaro">Desafios <br class="hidden sm:block"><span class="sm:text-sm font-normal">de la juventud</span></NuxtLink>
         <NuxtLink to="/actualidad"  active-class="text-mindaro" class="text-center leading-none hover:text-mindaro">Actualidad <br class="hidden sm:block"><span class="sm:text-sm font-normal">de la juventud</span></NuxtLink>
+        <NuxtLink to="/contacto" active-class="text-mindaro" class="text-center leading-none hover:text-mindaro">Contacto</NuxtLink>
+        <ULink to="https://linktr.ee/incidirparaexistir" target="_blank" class="text-center text-sm"><img src="/svg/linktree-logo-icon.svg" class="h-5" alt="Linktree"/></ULink>
       </div>
+      <hr class="opacity-60">
+      <p class="my-8 text-center text-sm">Desarrollado por <ULink to="https://democraciaenred.org" class="font-inter font-semibold underline hover:text-pumpkin-300" target="_blank">Democracia en Red</ULink></p>
     </UContainer>
   </footer>
 </template>
