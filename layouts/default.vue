687d566b7d634de3ee8dc9e1b31fbf6c42593902
Guillermo Croppi <guillermocroppi@gmail.com>
2025-02-21 12:23:00 -0300

    Simplify layout structure by removing unnecessary container and grid elements

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 </script>
 
+
 <template>
   <div class="flex h-screen flex-col">
     <div class="flex-grow flex-shrink basis-auto">
       <TheNavbar />
-      <UContainer class="my-9">
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-          <div class="col-span-1">
-            <UVerticalNavigation :links="links" class="sticky top-5 border rounded-lg border-slate-800 p-3" />
-          </div>
-          <div class="col-span-1 md:col-span-3"> 
-            <slot />
-          </div>
-        </div>
-      </UContainer>
+      <slot />
     </div>
     <TheFooter />
   </div>
