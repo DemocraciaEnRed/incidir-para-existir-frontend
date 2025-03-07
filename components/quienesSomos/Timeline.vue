d1545d047ad381dcb6395f87c640eed864735a16
Guillermo Croppi <guillermocroppi@gmail.com>
2025-03-07 18:25:15 -0300

    Add Timeline component to showcase historical social movements in Colombia

---
+++ components/quienesSomos/Timeline.vue
@@ -0,0 +1,56 @@
+<template>
+  
+<div class="ml-5 px-5 py-5">
+  <ol class="relative border-s border-gray-200 dark:border-gray-500">
+      <li class="mb-10 ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2019 - 2021</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">Los estallidos sociales históricos en Colombia</h3>
+          <p class="mb-4 text-base dark:text-white">En estos años, Colombia vivió una serie de estallidos sociales sin precedentes. Multitudinarias movilizaciones, plantones, tomas del espacio público y acciones simbólicas tanto online como offline canalizaron la inconformidad ciudadana.</p>
+          <p class="text-base dark:text-white">La juventud se destacó por su liderazgo, creatividad y valentía, siendo el motor de estas luchas colectivas. Sin embargo, también enfrentó graves vulneraciones como violencia física, desapariciones forzadas y tratos crueles e inhumanos.</p>
+          <!-- <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-6 h-6 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    </svg></a> -->
+      </li>
+      <li class="mb-10 ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2023</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">El nacimiento de "Incidir para Existir"</h3>
+          <p class="mb-4 text-base dark:text-white">Surge como un espacio para el encuentro de líderes y lideresas juveniles, promoviendo la formación y el diálogo con la institucionalidad.</p>
+          <p class="text-base dark:text-white">Con el objetivo de facilitar la incidencia política desde la escucha, la propuesta y la exigencia de demandas juveniles, con un enfoque en la dignificación de la vida y los sentires de los jóvenes de Cali y Bogotá.</p>
+      </li>
+      <li class="mb-10 ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2023</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">Creación de las agendas ciudadanas</h3>
+          <p class="text-base dark:text-white">A través de bootcamps y mentorías, se definieron colectivamente las demandas juveniles de ambas ciudades. Estas fueron consignadas en agendas ciudadanas que sirvieron como hoja de ruta para la incidencia política.</p>
+      </li>
+      <li class="mb-10 ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2024</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">Organización interna y trabajo colectivo</h3>
+          <p class="mb-4 text-base dark:text-white">Se fortalecieron los grupos juveniles en Cali y Bogotá, adaptando los procesos organizativos a las realidades sociopolíticas locales.</p>
+          <p class="text-base dark:text-white">La red comenzó a estructurarse bajo una lógica de macroprocesos, integrando diferentes vertientes necesarias para alcanzar los objetivos iniciales y materializar su teoría de cambio.</p>
+      </li>
+      <li class="mb-10 ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2024</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">Diálogo y transformación</h3>
+          <p class="mb-4 text-base dark:text-white">Funcionarios de las Alcaldías de Cali y Bogotá conocieron las agendas juveniles y se incluyeron propuestas importantes en los Planes de Desarrollo Locales.</p>
+          <p class="text-base dark:text-white">"Incidir para Existir" se posiciona como una plataforma de liderazgo juvenil, promoviendo la participación organizada y transformadora de los jóvenes en los contextos urbanos.</p>
+      </li>
+      <li class="mb-10 ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2024</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">Campañas de Incidencia</h3>
+          <p class="text-base dark:text-white">Como RED desarrollamos tres campañas importantes: La primera exigiendo la Tarifa diferencial en el transporte público para estudiantes en la ciudad de Cali, la segunda exigiendo condiciones de Trabajo Digno para las juventudes en Bogotá y la tercera en el marco de la COP-16 relacionada con la exigencia de defensa de la vida de los líderes socioambientales.</p>
+      </li>
+      <li class="ms-8 pl-7 pr-4 py-8 bg-gray-900 border border-gray-700 rounded-lg">
+          <div class="absolute w-6 h-6 bg-gray-200 rounded-full mt-0 -start-3 border border-white dark:border-gray-900 dark:bg-pumpkin-500"/>
+          <time class="mb-1 text-2xl font-semibold leading-none text-gray-400 dark:text-pumpkin-500">2025 (y más allá)</time>
+          <h3 class="text-3xl mb-2 font-oswald uppercase text-gray-900 dark:text-mindaro">Seguimos construyendo</h3>
+          <p class="text-base dark:text-white">Seguiremos construyendo ciudades más justas y dignas para las juventudes.</p>
+      </li>
+  </ol>
+</div>
+</template>
