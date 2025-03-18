b5c701b85077f94abc17d49cf3169fed6148a414
Guillermo Croppi <guillermocroppi@gmail.com>
2025-03-18 18:08:20 -0300

    Replace ULink with NuxtLink for improved routing in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -69,19 +69,19 @@ onMounted(() => {
             <div class="space-y-3">
               <div class="flex items-center">
                 <UButton to="/quienes-somos" icon="i-heroicons-arrow-right-circle" variant="soft" :ui="{ rounded: 'rounded-full' }" size="lg" class="mr-4" />
-                <p>Encontrar y conectar con una <ULink to="/quienes-somos" class="text-mindaro-500 underline">red de jóvenes</ULink> apasionados por crear cambio</p>
+                <p>Encontrar y conectar con una <NuxtLink to="/quienes-somos" class="text-mindaro-500 underline">red de jóvenes</NuxtLink> apasionados por crear cambio</p>
               </div>
               <div class="flex items-center">
                 <UButton to="/iniciativas" icon="i-heroicons-arrow-right-circle" variant="soft" :ui="{ rounded: 'rounded-full' }" size="lg" class="mr-4" />
-                <p>Ver y rastrear <ULink to="/iniciativas" class="text-mindaro-500 underline">iniciativas</ULink> juveniles, proyectos y acciones que buscan transformar la sociedad</p>
+                <p>Ver y rastrear <NuxtLink to="/iniciativas" class="text-mindaro-500 underline">iniciativas</NuxtLink> juveniles, proyectos y acciones que buscan transformar la sociedad</p>
               </div>
               <div class="flex items-center">
                 <UButton to="/desafios" icon="i-heroicons-arrow-right-circle" variant="soft" :ui="{ rounded: 'rounded-full' }" size="lg" class="mr-4" />
-                <p>Reportar y conocer los <ULink to="/desafios" class="text-mindaro-500 underline">desafíos</ULink> de la juventud en Bogotá y Cali</p>
+                <p>Reportar y conocer los <NuxtLink to="/desafios" class="text-mindaro-500 underline">desafíos</NuxtLink> de la juventud en Bogotá y Cali</p>
               </div>
               <div class="flex items-center">
                 <UButton to="/actualidad" icon="i-heroicons-arrow-right-circle" variant="soft" :ui="{ rounded: 'rounded-full' }" size="lg" class="mr-4" />
-                <p><ULink to="/actualidad" class="text-mindaro-500 underline">Descubrir</ULink> las voces y en qué anda la juventud caleña y bogotana</p>
+                <p><NuxtLink to="/actualidad" class="text-mindaro-500 underline">Descubrir</NuxtLink> las voces y en qué anda la juventud caleña y bogotana</p>
               </div>
             </div>
             <p class="font-medium italic">Únete a nuestra comunidad y sé parte del cambio que queremos ver en el mundo</p>
@@ -94,7 +94,7 @@ onMounted(() => {
       
     <div class="space-y-7 my-16 lg:w-10/12 mx-auto">
       <div class="w-full text-center">
-          <p class="font-bold text-xl md:text-2xl">Este es un proyecto de <ULink to="https://movilizatorio.org" class="text-mindaro underline">Movilizatorio</ULink> apoyado por el <ULink to="https://www.un.org/democracyfund/" class="text-mindaro underline">Fondo de las Naciones Unidas para la Democracia</ULink></p>
+          <p class="font-bold text-xl md:text-2xl">Este es un proyecto de <NuxtLink to="https://movilizatorio.org" class="text-mindaro underline">Movilizatorio</NuxtLink> apoyado por el <NuxtLink to="https://www.un.org/democracyfund/" class="text-mindaro underline">Fondo de las Naciones Unidas para la Democracia</NuxtLink></p>
       </div>
     </div>
     <UCarousel v-slot="{ item }" :items="imgPhotoCarrousel" :ui="{ wrapper: 'relative px-10',item: 'basis-full md:basis-1/2 lg:basis-1/4 my-3 mx-4 rounded-lg' }" arrows>
